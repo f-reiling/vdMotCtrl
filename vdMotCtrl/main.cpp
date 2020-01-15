2f2141d8ae528b26bf47a180f9676e70b6772fe2
Florian Reiling <florian.reiling@gmail.com>
2020-01-15 20:18:10 +0100

    initial project setup

---
+++ vdMotCtrl/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * vdMotCtrl.cpp
+ *
+ * Created: 15.01.2020 20:06:48
+ * Author : Florian
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
